84b6ada36a23f65c68e8bba5c1c6397d308e055b
Gea-Suan Lin <gslin@gslin.org>
2016-07-31 09:00:21 +0800

    Implemt whitelist settings.

--- src/options.js
+++ src/options.js
@@ -3,18 +3,39 @@
 (function() {
     new Vue({
         el: '#app',
-        data: {},
+        data: {
+            whitelist_hosts: [],
+        },
         methods: {
             addHostname: function() {
                 var h = this.new_hostname.trim();
                 if (-1 === this.whitelist_hosts.indexOf(h)) {
                     this.whitelist_hosts.push(h);
+
+                    var that = this;
+                    chrome.storage.sync.set({
+                        whitelist_hosts: that.whitelist_hosts,
+                    });
                 }
                 this.new_hostname = '';
             },
             deleteHostname: function(idx) {
                 this.whitelist_hosts.splice(idx, 1);
+
+                var that = this;
+                chrome.storage.sync.set({
+                    whitelist_hosts: that.whitelist_hosts,
+                });
             },
         },
+        ready: function() {
+            var that = this;
+
+            chrome.storage.sync.get({
+                whitelist_hosts: [],
+            }, function(items) {
+                that.whitelist_hosts = items.whitelist_hosts;
+            });
+        }
     });
 })();
