9d2f30707ce0956c2e4527af9805d940fd4771a6
Gea-Suan Lin <gslin@gslin.org>
2016-07-31 08:04:25 +0800

    Support blocking space key, and support whitelist.

--- content.js
+++ content.js
@@ -1,6 +1,12 @@
 'use strict';
 
 (function(){
+    var whitelist_hosts = ['www.google.com'];
+
+    if (-1 !== whitelist_hosts.indexOf(document.location.hostname)) {
+        return;
+    }
+
     document.addEventListener('DOMContentLoaded', function(){
         [document, window].forEach(function(target){
             ['mousewheel', 'wheel'].forEach(function(eventName){
@@ -11,9 +17,9 @@
 
             target.addEventListener('keydown', function(event){
                 /*
-                 * 33 pageup, 34 pagedown, 35 end, 36 home, 38 up, 40 down
+                 * 32 space, 33 pageup, 34 pagedown, 35 end, 36 home, 38 up, 40 down
                  */
-                if (-1 !== [33, 34, 35, 36, 38, 40].indexOf(event.which)) {
+                if (-1 !== [32, 33, 34, 35, 36, 38, 40].indexOf(event.which)) {
                     console.log(event.which);
                     event.stopPropagation();
                 }
