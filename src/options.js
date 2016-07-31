851a64893b255386731142a2c0eb5d762d346853
Gea-Suan Lin <gslin@gslin.org>
2016-07-31 08:44:20 +0800

    Add addHostname.

--- src/options.js
+++ src/options.js
@@ -5,6 +5,13 @@
         el: '#app',
         data: {},
         methods: {
+            addHostname: function() {
+                var h = this.new_hostname.trim();
+                if (-1 === this.whitelist_hosts.indexOf(h)) {
+                    this.whitelist_hosts.push(h);
+                }
+                this.new_hostname = '';
+            },
             deleteHostname: function(idx) {
                 this.whitelist_hosts.splice(idx, 1);
             },
