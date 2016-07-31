f5a967ffa081fc2f774514f8cb87e3826caa7660
Gea-Suan Lin <gslin@gslin.org>
2016-07-31 09:34:49 +0800

    Indent.

--- src/content.js
+++ src/content.js
@@ -13,24 +13,24 @@
     });
 
     p.then(function() {
-    document.addEventListener('DOMContentLoaded', function(){
-        [document, window].forEach(function(target){
-            ['mousewheel', 'wheel'].forEach(function(eventName){
-                target.addEventListener(eventName, function(e){
-                    e.stopPropagation();
+        document.addEventListener('DOMContentLoaded', function(){
+            [document, window].forEach(function(target){
+                ['mousewheel', 'wheel'].forEach(function(eventName){
+                    target.addEventListener(eventName, function(e){
+                        e.stopPropagation();
+                    }, true);
+                });
+
+                target.addEventListener('keydown', function(event){
+                    /*
+                     * 32 space, 33 pageup, 34 pagedown, 35 end, 36 home, 38 up, 40 down
+                     */
+                    if (-1 !== [32, 33, 34, 35, 36, 38, 40].indexOf(event.which)) {
+                        console.log(event.which);
+                        event.stopPropagation();
+                    }
                 }, true);
             });
-
-            target.addEventListener('keydown', function(event){
-                /*
-                 * 32 space, 33 pageup, 34 pagedown, 35 end, 36 home, 38 up, 40 down
-                 */
-                if (-1 !== [32, 33, 34, 35, 36, 38, 40].indexOf(event.which)) {
-                    console.log(event.which);
-                    event.stopPropagation();
-                }
-            }, true);
         });
-    });
     });
 })();
