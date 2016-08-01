9ebccac92c8b14cdd74e50464295af423ed92f8a
Gea-Suan Lin <gslin@gslin.org>
2016-08-01 11:31:36 +0800

    Add mail.google.com.

--- src/options.js
+++ src/options.js
@@ -2,6 +2,7 @@
 
 (function() {
     var default_whitelist_hosts = [
+        'mail.google.com',              // Gmail
         'www.bing.com',                 // Bing Maps
         'www.google.com',               // Google Maps
         'www.netflix.com',              // Netflix
