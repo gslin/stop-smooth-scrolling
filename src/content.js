a4834fca8afbf85fe13021a675aa1bd7d50ba03e
Gea-Suan Lin <gslin@gslin.org>
2024-12-30 13:39:40 +0800

    refactor: indent.

--- src/content.js
+++ src/content.js
@@ -1,50 +1,50 @@
 'use strict';
 
 (function(){
-    var p = new Promise(function(resolve) {
-        chrome.storage.sync.get({
-            whitelist_hosts: [],
-        }, function(items) {
-            let cnt = items.whitelist_hosts.length;
-            for (let i = 0; i < cnt; i++) {
-                let h = items.whitelist_hosts[i];
-                if (document.location.hostname === h) {
-                    return;
-                }
-                if (h.startsWith('.') && document.location.hostname.endsWith(h)) {
-                    return;
-                }
-            }
+  var p = new Promise(function(resolve) {
+    chrome.storage.sync.get({
+      whitelist_hosts: [],
+    }, function(items) {
+      let cnt = items.whitelist_hosts.length;
+      for (let i = 0; i < cnt; i++) {
+        let h = items.whitelist_hosts[i];
+        if (document.location.hostname === h) {
+          return;
+        }
+        if (h.startsWith('.') && document.location.hostname.endsWith(h)) {
+          return;
+        }
+      }
 
-            resolve();
-        });
+      resolve();
     });
+  });
 
-    p.then(function() {
-        document.addEventListener('DOMContentLoaded', function(){
-            [document, window].forEach(function(target){
-                ['mousewheel', 'wheel'].forEach(function(eventName){
-                    target.addEventListener(eventName, function(e){
-                        e.stopPropagation();
-                    }, true);
-                });
-
-                target.addEventListener('keydown', function(event){
-                    /*
-                     * Don't block keydown event in input & textarea.
-                     */
-                    if (document.activeElement && -1 !== ['input', 'textarea'].indexOf(document.activeElement.tagName.toLowerCase())) {
-                        return;
-                    }
-
-                    /*
-                     * 32 space, 33 pageup, 34 pagedown, 35 end, 36 home, 38 up, 40 down
-                     */
-                    if (-1 !== [32, 33, 34, 35, 36, 38, 40].indexOf(event.which)) {
-                        event.stopPropagation();
-                    }
-                }, true);
-            });
+  p.then(function() {
+    document.addEventListener('DOMContentLoaded', function(){
+      [document, window].forEach(function(target){
+        ['mousewheel', 'wheel'].forEach(function(eventName){
+          target.addEventListener(eventName, function(e){
+            e.stopPropagation();
+          }, true);
         });
+
+        target.addEventListener('keydown', function(event){
+          /*
+           * Don't block keydown event in input & textarea.
+           */
+          if (document.activeElement && -1 !== ['input', 'textarea'].indexOf(document.activeElement.tagName.toLowerCase())) {
+            return;
+          }
+
+          /*
+           * 32 space, 33 pageup, 34 pagedown, 35 end, 36 home, 38 up, 40 down
+           */
+          if (-1 !== [32, 33, 34, 35, 36, 38, 40].indexOf(event.which)) {
+            event.stopPropagation();
+          }
+        }, true);
+      });
     });
+  });
 })();
--- src/options.js
+++ src/options.js
@@ -1,76 +1,76 @@
 'use strict';
 
 (function() {
-    var default_whitelist_hosts = [
-        'docs.google.com',              // Google Docs
-        'mail.google.com',              // Gmail
-        '.slack.com',                   // Slack
-        'www.bing.com',                 // Bing Maps
-        'www.google.com',               // Google Maps
-        'www.netflix.com',              // Netflix
-        'www.openstreetmap.org',        // OpenStreetMaps
-        'www.plurk.com',                // Plurk
-    ];
+  var default_whitelist_hosts = [
+    'docs.google.com',              // Google Docs
+    'mail.google.com',              // Gmail
+    '.slack.com',                   // Slack
+    'www.bing.com',                 // Bing Maps
+    'www.google.com',               // Google Maps
+    'www.netflix.com',              // Netflix
+    'www.openstreetmap.org',        // OpenStreetMaps
+    'www.plurk.com',                // Plurk
+  ];
 
-    new Vue({
-        el: '#app',
-        data: {
-            whitelist_hosts: [],
-        },
-        methods: {
-            addHostname: function() {
-                var h = this.new_hostname.trim();
-                if ('' === h) {
-                    alert('Empty hostname is not acceptable');
-                    return;
-                }
-
-                if (-1 !== this.whitelist_hosts.indexOf(h)) {
-                    alert(h + ' already exists');
-                    return;
-                }
-
-                this.whitelist_hosts.push(h);
-
-                var that = this;
-                chrome.storage.sync.set({
-                    whitelist_hosts: that.whitelist_hosts,
-                });
-
-                this.new_hostname = '';
-            },
-            deleteHostname: function(idx) {
-                this.whitelist_hosts.splice(idx, 1);
-
-                var that = this;
-                chrome.storage.sync.set({
-                    whitelist_hosts: that.whitelist_hosts,
-                });
-            },
-            resetToDefault: function() {
-                var yn = confirm('Reset all whitelist settings?');
-                if (!yn) {
-                    return;
-                }
-
-                this.whitelist_hosts = default_whitelist_hosts;
-
-                var that = this;
-                chrome.storage.sync.set({
-                    whitelist_hosts: that.whitelist_hosts,
-                }, function() {
-                    document.location.reload();
-                });
-            }
-        },
-        ready: function() {
-            var that = this;
-
-            chrome.storage.sync.get({
-                whitelist_hosts: default_whitelist_hosts,
-            }, function(items) {
-                that.whitelist_hosts = items.whitelist_hosts;
-            });
+  new Vue({
+    el: '#app',
+    data: {
+      whitelist_hosts: [],
+    },
+    methods: {
+      addHostname: function() {
+        var h = this.new_hostname.trim();
+        if ('' === h) {
+          alert('Empty hostname is not acceptable');
+          return;
         }
-    });
+
+        if (-1 !== this.whitelist_hosts.indexOf(h)) {
+          alert(h + ' already exists');
+          return;
+        }
+
+        this.whitelist_hosts.push(h);
+
+        var that = this;
+        chrome.storage.sync.set({
+          whitelist_hosts: that.whitelist_hosts,
+        });
+
+        this.new_hostname = '';
+      },
+      deleteHostname: function(idx) {
+        this.whitelist_hosts.splice(idx, 1);
+
+        var that = this;
+        chrome.storage.sync.set({
+          whitelist_hosts: that.whitelist_hosts,
+        });
+      },
+      resetToDefault: function() {
+        var yn = confirm('Reset all whitelist settings?');
+        if (!yn) {
+          return;
+        }
+
+        this.whitelist_hosts = default_whitelist_hosts;
+
+        var that = this;
+        chrome.storage.sync.set({
+          whitelist_hosts: that.whitelist_hosts,
+        }, function() {
+          document.location.reload();
+        });
+      }
+    },
+    ready: function() {
+      var that = this;
+
+      chrome.storage.sync.get({
+        whitelist_hosts: default_whitelist_hosts,
+      }, function(items) {
+        that.whitelist_hosts = items.whitelist_hosts;
+      });
+    }
+  });
 })();
